3bec3ec667ef234e0298ea38db88016cf29a1552
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-10-05 03:02:58 -0500

    Update ESP32CAM.ino

--- ESP32CAM/ESP32CAM.ino
+++ ESP32CAM/ESP32CAM.ino
@@ -36,13 +36,13 @@ const char* ssid = "FAMILIA PC";
 const char* password = "L782019JT";
 
 // Dirección del servidor o dirección IP del servidor
-const char* serverName = "192.168.1.21";
+const char* serverName = "192.168.1.13";
 
 // Ruta del archivo en el servidor
-const char* serverPath = "/compare";
+const char* serverPath = "/add_person";
 
 // Puerto del servidor
-const int serverPort = 8000;
+const int serverPort = 5000;
 
 // Variable para controlar si el flash LED está encendido o apagado
 
