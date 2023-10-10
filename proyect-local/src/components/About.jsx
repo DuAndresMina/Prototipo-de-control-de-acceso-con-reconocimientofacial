57166294777d7a6d8403acd08f4911b2f1afc849
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-10-09 22:05:51 -0500

    Create About.jsx

---
+++ proyect-local/src/components/About.jsx
@@ -0,0 +1,11 @@
+// src/components/About.js
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Learn more about us!</p>
+    </div>
+  )
+}
