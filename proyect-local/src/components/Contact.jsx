8fb372a211e5873c097c49a38b8b541b5754119b
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-10-09 22:05:54 -0500

    Create Contact.jsx

---
+++ proyect-local/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+// src/components/Contact.js
+import React from 'react';
+
+export default function dadsa() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Get in touch with us!</p>
+    </div>
+  )
+}
