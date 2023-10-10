3acdd1432a62da42f1292c9ebe54b23472f43003
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-10-09 22:05:56 -0500

    Create Home.jsx

---
+++ proyect-local/src/components/Home.jsx
@@ -0,0 +1,11 @@
+// src/components/Home.js
+import React from 'react';
+
+export default function asdasd() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to our home page!</p>
+    </div>
+  )
+}
