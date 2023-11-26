4c55ae3f35ab719da0d179a439ee7a7ed4bea7cb
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-11-25 21:49:45 -0500

    Update Home2.js

--- proyect-page/login-app-reactjs-master/src/Home2.js
+++ proyect-page/login-app-reactjs-master/src/Home2.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import { getUser, removeUserSession } from './Utils/Common';
+
+// Crear un componente Styled para el contenedor principal
+const Container = styled.div`
+  text-align: center;
+  padding: 20px;
+  background-color: #f0f0f0;
+`;
+
+// Crear un componente Styled para el título
+const Title = styled.h1`
+  color: #333;
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+// Crear un componente Styled para el botón de cierre de sesión
+const LogoutButton = styled.button`
+  background-color: #e44d26;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #d04023;
+  }
+`;
 
 function Home2(props) {
   const user = getUser(); // Obtén los detalles del usuario desde el almacenamiento local
@@ -11,11 +41,11 @@ function Home2(props) {
   }
 
   return (
-    <div>
-      <h1>Welcome, {user.name}!</h1>
-      <button onClick={handleLogout}>Logout</button>
+    <Container>
+      <Title>Welcome, {user.name}!</Title>
+      <LogoutButton  onClick={handleLogout}>Logout</LogoutButton >
       {/* Agrega aquí la interfaz que deseas mostrar después de iniciar sesión */}
-    </div>
+    </Container>
   );
 }
 
