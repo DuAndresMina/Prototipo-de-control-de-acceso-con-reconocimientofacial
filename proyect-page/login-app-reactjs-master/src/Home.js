a801fcb91f71edafc0642357c8445b8e66095115
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-12-07 03:43:18 -0500

    Update Home.js

--- proyect-page/login-app-reactjs-master/src/Home.js
+++ proyect-page/login-app-reactjs-master/src/Home.js
@@ -1,76 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
 import { setUserSession } from './Utils/Common';
 import { getUser, removeUserSession } from './Utils/Common';
-import Home2 from './Home2';
-
-// Crear un componente Styled para el contenedor principal
-const Container = styled.div`
-  text-align: center;
-  padding: 20px;
-  background-color: #f0f0f0;
-`;
-
-// Crear un componente Styled para el título
-const Title = styled.h1`
-  color: #333;
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-// Crear un componente Styled para el formulario
-const Form = styled.div`
-  margin-top: 20px;
-`;
-
-// Crear un componente Styled para los campos de entrada
-const InputField = styled.div`
-  margin-bottom: 10px;
-`;
-
-// Crear un componente Styled para el botón de inicio de sesión
-const LoginButton = styled.input`
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
-
-// Crear un componente Styled para el mensaje de error
-const ErrorMessage = styled.small`
-  color: red;
-`;
+import { Container, Typography, TextField, Button, Box } from '@mui/material';
 
 function Home(props) {
-  // Estados del componente
   const [loading, setLoading] = useState(false);
   const username = useFormInput('');
   const password = useFormInput('');
   const [error, setError] = useState(null);
-  const user = getUser(); // Obtén los detalles del usuario desde el almacenamiento local
+  const user = getUser();
 
-  // Redirigir si el usuario ya está autenticado
   useEffect(() => {
     if (user) {
       props.history.push('/home-2');
     }
   }, [props.history, user]);
 
-  // Manejar el inicio de sesión
   const handleLogin = () => {
     setError(null);
     setLoading(true);
 
-
-    // Hacer la llamada a la API para iniciar sesión
     axios
       .post('http://localhost:4000/users/signin', {
         username: username.value,
@@ -86,63 +36,71 @@ function Home(props) {
         if (error.response && error.response.status === 400) {
           setError(error.response.data.message);
         } else {
-          setError("Something went wrong. Please try again later.");
+          setError('Something went wrong. Please try again later.');
         }
       });
-  }
+  };
 
-    // Manejar el cierre de sesión
-    const handleLogout = () => {
-      removeUserSession();
-      props.history.push('/');
-    }
+  const handleLogout = () => {
+    removeUserSession();
+    props.history.push('/');
+  };
 
   return (
     <Container>
       {user ? (
-        // Si el usuario ya ha iniciado sesión, muestra el mensaje de bienvenida y el botón de cierre de sesión
-        <div>
-          <Title>Welcome, {user.name}!</Title>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
+        <Box textAlign="center">
+          <Typography variant="h4">Welcome, {user.name}!</Typography>
+          <Button variant="contained" color="primary" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Box>
       ) : (
-        // Si no ha iniciado sesión, muestra el formulario de inicio de sesión
-        <div>
-          <Title>Welcome to the Home Page!</Title>
-          <Form>
-            <InputField>
-              <div>
-                Username<br />
-                <input type="text" {...username} autoComplete="new-password" />
-              </div>
-            </InputField>
-            <InputField>
-              <div>
-                Password<br />
-                <input type="password" {...password} autoComplete="new-password" />
-              </div>
-            </InputField>
-            {error && <ErrorMessage>{error}</ErrorMessage>}
-            <LoginButton type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} />
-          </Form>
-        </div>
+        <Box textAlign="center">
+          <Typography variant="h4">Welcome to the Home Page!</Typography>
+          <Box component="form" sx={{ mt: 2 }}>
+            <TextField
+              label="Username"
+              type="text"
+              {...username}
+              autoComplete="new-password"
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              {...password}
+              autoComplete="new-password"
+              sx={{ mb: 2 }}
+            />
+            {error && <Typography color="error">{error}</Typography>}
+            <Button
+              type="button"
+              variant="contained"
+              color="primary"
+              onClick={handleLogin}
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Login'}
+            </Button>
+          </Box>
+        </Box>
       )}
     </Container>
   );
 }
 
-// Función para manejar los campos de entrada del formulario
 const useFormInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
 
   const handleChange = (e) => {
     setValue(e.target.value);
-  }
+  };
 
   return {
     value,
     onChange: handleChange,
   };
-}
+};
 
 export default Home;
