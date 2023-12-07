437ebedbf605ec56a8948a636011ba7b140b7ad6
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2023-12-07 03:43:15 -0500

    Update App.js

--- proyect-page/login-app-reactjs-master/src/App.js
+++ proyect-page/login-app-reactjs-master/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css'; // Importa tu archivo de estilos principal
 import Login from './Login';
 import Personas from './Personas_data';
 import Autenticaciones from './Personas_auten'
 import Personas_fallos from './Personas_fallos';
+import AgregarPersona from './AgregarPersona'; // Ajusta la ruta según tu estructura de carpetas
 import Home from './Home';
 import Navbar from './Utils/Navbar'; // Asegúrate de importar el componente Navbar
 import Home2 from './Home2'
@@ -48,6 +49,7 @@ function App() {
               <PrivateRoute path="/Usuarios" component={Personas} />
               <PrivateRoute path="/Registros" component={Autenticaciones} />
               <PrivateRoute path="/Invalidos" component={Personas_fallos} />
+              <PrivateRoute path="/Agregar_personas" component={AgregarPersona} />
             </Switch>
           </div>
         </div>
