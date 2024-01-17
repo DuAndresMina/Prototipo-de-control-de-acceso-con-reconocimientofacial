983a93231db51352bd469c2a34273cad81838cfd
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2024-01-17 13:53:39 -0500

    Update Common.js

--- proyect-page/login-app-reactjs-master/src/Utils/Common.js
+++ proyect-page/login-app-reactjs-master/src/Utils/Common.js
@@ -7,8 +7,27 @@ export const getUser = () => {
 
 // return the token from the session storage
 export const getToken = () => {
-  return sessionStorage.getItem('token') || null;
-}
+  const token = sessionStorage.getItem('token');
+  const tokenExp = sessionStorage.getItem('tokenExp');
+
+  if (!token || !tokenExp) {
+    console.log('No hay token o tiempo de expiración.');
+    return null;
+  }
+
+  const currentTime = new Date().getTime() / 1000; // tiempo actual en segundos
+  console.log('Tiempo actual:', currentTime);
+  console.log('Tiempo de expiración del token:', tokenExp);
+
+  if (currentTime > tokenExp) {
+    console.log('Token expirado. Eliminando token y tiempo de expiración.');
+    removeUserSession();
+    return null;
+  }
+
+  console.log('Token válido.');
+  return token;
+};
 
 // remove the token and user from the session storage
 export const removeUserSession = () => {
@@ -16,8 +35,14 @@ export const removeUserSession = () => {
   sessionStorage.removeItem('user');
 }
 
-// set the token and user from the session storage
 export const setUserSession = (token, user) => {
   sessionStorage.setItem('token', token);
+
+  // Calcular el tiempo de expiración del token (por ejemplo, 30 segundos desde ahora)
+  const tokenExp = new Date().getTime() / 1000 + 600; // 30 segundos de duración
+
+  console.log('Tiempo de expiración del token:', tokenExp);
+
+  sessionStorage.setItem('tokenExp', tokenExp);
   sessionStorage.setItem('user', JSON.stringify(user));
-}
+};
