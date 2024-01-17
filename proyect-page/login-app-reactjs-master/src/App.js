9dbb67947f133ad3baf741fe561ea7b82682d7ec
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2024-01-17 13:53:15 -0500

    Update App.js

--- proyect-page/login-app-reactjs-master/src/App.js
+++ proyect-page/login-app-reactjs-master/src/App.js
@@ -5,7 +5,6 @@ import './App.css'; // Importa tu archivo de estilos principal
 import Login from './Login';
 import Personas from './Personas_data';
 import Autenticaciones from './Personas_auten'
-import Personas_fallos from './Personas_fallos';
 import AgregarPersona from './AgregarPersona'; // Ajusta la ruta según tu estructura de carpetas
 import Home from './Home';
 import Navbar from './Utils/Navbar'; // Asegúrate de importar el componente Navbar
@@ -19,20 +18,34 @@ function App() {
 
   useEffect(() => {
     const token = getToken();
+    console.log('Token al inicio:', token);
+
     if (!token) {
+      console.log('No hay token. Saliendo.');
+      setAuthLoading(false);
       return;
     }
+    const serverIp = '192.168.20.2';
+    axios.get(`http://${serverIp}:4000/verifyToken?token=${token}`).then(response => {
+      console.log('Respuesta del servidor:', response);
 
-    axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(response => {
-      setUserSession(response.data.token, response.data.user);
-      setAuthLoading(false);
+      if (response.data && response.data.token && response.data.user) {
+        setUserSession(response.data.token, response.data.user);
+        console.log('Token y usuario almacenados. Desactivando carga de autenticación.');
+        setAuthLoading(false);
+      } else {
+        console.log('La respuesta del servidor no contiene token o usuario.');
+        removeUserSession();
+        setAuthLoading(false);
+      }
     }).catch(error => {
+      console.error('Error al verificar el token:', error);
       removeUserSession();
       setAuthLoading(false);
     });
   }, []);
 
-  if (authLoading && getToken()) {
+  if (authLoading) {
     return <div className="content">Checking Authentication...</div>
   }
 
@@ -40,7 +53,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <div>
-          <Navbar /> {/* Asegúrate de incluir el componente Navbar dentro de BrowserRouter */}
+          <Navbar />
           <div className="content">
             <Switch>
               <Route exact path="/" component={Home} />
@@ -48,7 +61,6 @@ function App() {
               <PublicRoute path="/login" component={Login} />
               <PrivateRoute path="/Usuarios" component={Personas} />
               <PrivateRoute path="/Registros" component={Autenticaciones} />
-              <PrivateRoute path="/Invalidos" component={Personas_fallos} />
               <PrivateRoute path="/Agregar_personas" component={AgregarPersona} />
             </Switch>
           </div>
