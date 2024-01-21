8c5b4c59d0597f6a20eb3fff62da22d57a3a055a
MrDuvancho <129365067+MrDuvancho@users.noreply.github.com>
2024-01-20 19:32:37 -0500

    Create webpack.config.js

---
+++ proyect-page/login-app-reactjs-master/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = function (webpackEnv) {
+    // ...
+    return {
+     // ...
+      resolve: {
+        // ...
+        fallback: {
+
+          crypto: require.resolve("crypto-browserify"),
+          stream: require.resolve("stream-browserify"),
+        }
+      }
+    }
+  }
+  
